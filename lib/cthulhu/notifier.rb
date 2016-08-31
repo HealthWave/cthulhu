fef52f8a8d5d9edfea4fe7a1adb9749611a17a2d
patvice <patrickgvice@gmail.com>
2016-08-31 09:33:43 -0400

    added new class method for changed attribute functionality

--- lib/cthulhu/notifier.rb
+++ lib/cthulhu/notifier.rb
@@ -5,37 +5,21 @@ module Cthulhu
     end
 
     module ClassMethods
-      ACTION_MAP = { destroy: "after_destroy", create: 'after_save', update: 'after_save' }
-
       def cthulhu_notify(options={})
-        changed = options.delete(:on_change)
-
-        on = options.delete(:on)
-        on = ACTION_MAP.keys if on.nil? && changed.nil?
-
         include Cthulhu::Notifier::InstanceMethods
-        on.each do |o|
-          method = ACTION_MAP[o.to_sym]
-          next if method.nil?
+        @notifier = Notifier.new(self, options)
 
-          action = o.last == 'e' ? "#{o}d" : "#{o}ed"
-          self.send( method ) { |model| model.cthulhu_publish(action, options) }
-        end
-
-        changed.each do |attribute|
-          before_save do |model|
-            return unless model.send("#{attribute}_changed?")
-            payload = {
-              "#{model.class.name}_id" => model.id,
-              to: model.send(attribute),
-              from: model.send("#{attribute}_was",
-              datetime: DateTime.now
-            }
-
-            model.cthulhu_publish( "#{attribute}_updated", options.merge({payload: payload}) )
-          end
-        end
+        @notifier.setup_lifecyle_events!
       end
+
+      def cthulhu_changed(options={}, &block)
+        include Cthulhu::Notifier::InstanceMethods
+        options[:payload] = block if block_given?
+        @notifier = Notifier.new(self, options)
+
+        @notifier.setup_change_events!
+      end
+
     end
 
     module InstanceMethods
@@ -44,5 +28,66 @@ module Cthulhu
         Cthulhu.publish(subject: self.class.name, action: action, options: options, payload: payload || self.attributes )
       end
     end
+
+    class Notifier
+      ACTION_MAP = { destroy: "after_destroy", create: 'after_save', update: 'after_save' }
+
+      attr_reader :changed, :on, :options, :klass, :payload
+
+      def initialize(klass, options={})
+        @klass = klass
+        @changed = options.delete(:on_change)
+
+        @on = options.delete(:on)
+        @on = ACTION_MAP.keys if on.nil? && changed.nil?
+
+        @options = options
+      end
+
+      def setup_lifecyle_events!
+        on.each do |o|
+          method = ACTION_MAP[o.to_sym]
+          next if method.nil?
+          self.send( method ) { |instance| instance.cthulhu_publish( past_tensify(aciton), options ) }
+        end
+      end
+
+      def setup_change_events!
+        changed.each do |attribute|
+          before_save do |instance|
+            return unless instance.send("#{attribute}_changed?")
+            payload = generate_payload( instance, attribute, options.delete(:payload) )
+
+            instance.cthulhu_publish( "#{attribute}_updated", options.merge({payload: payload}) )
+          end
+        end
+      end
+
+      private
+        def past_tensify(action)
+          action = o.last == 'e' ? "#{o}d" : "#{o}ed"
+        end
+
+        def generate_payload(instance, attribute, payload)
+          case
+          when payload.respond_to? :call
+            payload.call
+          when payload.is_a? Symbol, payload.is_a? String
+            instance.send(payload)
+          else
+            default_payload(instance, attribute)
+          end
+        end
+
+        def default_payload(instance, attribute)
+          return {
+            "#{instance.class.name}_id" => instance.id,
+            "to" => instance.send(attribute),
+            "from" => instance.send("#{attribute}_was"),
+            "datetime" => DateTime.now
+          }
+        end
+    end
+
   end
 end
